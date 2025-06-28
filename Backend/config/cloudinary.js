b3298018903e11b9c5c7714b6129ce2c27263ad0
Sujay <sujaypowar13@gmail.com>
2025-06-28 16:28:30 +0530

    Initialized Backend and its setup

---
+++ Backend/config/cloudinary.js
@@ -0,0 +1,13 @@
+import { v2 as cloudinary } from "cloudinary";
+
+const connectCloudinary = async () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+  });
+};
+
+export default connectCloudinary;
+// This code configures Cloudinary with the credentials stored in environment variables.
+// It exports a function that initializes the Cloudinary configuration, which can be called in the main
