1199d94771d0b32c3fe8ecc86d86061a4c3e5360
Sujay <sujaypowar13@gmail.com>
2025-06-30 23:45:18 +0530

    Added Admit Panel and add-docotr API is setup

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,8 +1,17 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 // eslint-disable-next-line react-refresh/only-export-components
 export const AdminContext = createContext();
 const AdminContextProvider = (props) => {
-  const value = {};
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const value = {
+    aToken,
+    setAToken,
+    backendUrl,
+  };
   return (
     <AdminContext.Provider value={value}>
       {props.children}
