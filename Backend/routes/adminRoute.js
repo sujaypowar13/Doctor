12a8e1c5ee8daf289c563fcc1d42778891b79ff0
Sujay <sujaypowar13@gmail.com>
2025-07-01 20:53:43 +0530

    Fixed API for ading doctor using UI

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,13 +1,20 @@
 import express from "express";
-import { addDoctor, loginAdmin } from "../controllers/adminController.js";
+import {
+  addDoctor,
+  allDoctors,
+  loginAdmin,
+} from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
+import { changeAvailability } from "../controllers/doctorController.js";
 
 const adminRouter = express.Router();
 
 // Route to add a doctor
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
+adminRouter.post("/all-doctors", authAdmin, allDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailability);
 export default adminRouter;
 // This code sets up an Express router for admin-related routes.
 // It imports the necessary modules and defines a route for adding a doctor.
