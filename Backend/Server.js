c15bfc040e0cd422d4dd2a76b56f7a66a5114cc5
Sujay <sujaypowar13@gmail.com>
2025-07-02 20:36:39 +0530

    Fixed API and connected to UI

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,6 +4,8 @@ import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,6 +18,10 @@ app.use(cors());
 // API endpoints
 app.use("/api/admin", adminRouter);
 //localhost:4000/api/admin/add-doctor
+app.use("/api/doctor", doctorRouter);
+//localhost:4000/api/admin/all-doctor
+app.use("/api/user", userRouter);
+//localhost:4000/api/user/register
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Sujay Backend Server!");
