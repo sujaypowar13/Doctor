7f3b5fe187f705154f1458081e441fb46ebe90ed
sujaypowar13 <sujaypowar13@gmail.com>
2025-07-27 15:39:12 +0530

    fixed UI with API

--- Frontend/src/pages/MyAppointments.jsx
+++ Frontend/src/pages/MyAppointments.jsx
@@ -44,6 +44,25 @@ const MyAppointments = () => {
     }
   };
 
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/user/cancel-appointment",
+        { appointmentId },
+        { headers: { token } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getUserAppointments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     if (token) {
       getUserAppointments();
@@ -88,7 +107,10 @@ const MyAppointments = () => {
               <button className="cursor-pointer text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-[#5f6FFF] hover:text-white transition-all duration-300">
                 Pay Online
               </button>
-              <button className="cursor-pointer text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300">
+              <button
+                onClick={() => cancelAppointment(item._id)}
+                className="cursor-pointer text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300"
+              >
                 Cancel appointment
               </button>
             </div>
